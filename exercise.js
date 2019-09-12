9bc50304711f2a5efd1848e7aa17bc7db9457687
sabrina2712 <mailroshni@gmail.com>
2019-09-12 06:25:32 -0400

    add solution 1-5

---
+++ exercise.js
@@ -0,0 +1,13 @@
+const a = "true";
+const b = "false";
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a && b);
+
+const x = 5;
+const y = 7;
+const z = 9;
+
+console.log(x > z);
+console.log(x > y);
