322fec48125abab14e331e5f6f6cb8c96e9cd722
sabrina2712 <mailroshni@gmail.com>
2019-09-12 07:07:02 -0400

    add solution from comparison operation

--- exercise.js
+++ exercise.js
@@ -17,3 +17,21 @@ console.log(x === z || !x === y);
 console.log(x >= 10 && y <= 10);
 console.log(x * z < 100 || x * y > 100);
 console.log(x * z < 100 && x * y > 100);
+
+// comparison operation exercise
+
+const p = 20;
+const q = 30;
+const r = 5;
+
+console.log(p == q);
+console.log(p !== q);
+console.log(p > q);
+console.log(p <= q);
+console.log(p < q);
+console.log(p * r > q * r);
+console.log(p - r < q / r);
+console.log(p == q && q == r && r == p);
+console.log(p % r == q % r);
+console.log(r + p > q - r);
+console.log(r + p >= q - r);
