97b68970356f781db9732a11c3c2acca5d2855a4
sabrina2712 <mailroshni@gmail.com>
2019-09-12 06:39:38 -0400

    add solution 6-10

--- exercise.js
+++ exercise.js
@@ -11,3 +11,9 @@ const z = 9;
 
 console.log(x > z);
 console.log(x > y);
+console.log(!x === y);
+console.log(x < y || z > x);
+console.log(x === z || !x === y);
+console.log(x >= 10 && y <= 10);
+console.log(x * z < 100 || x * y > 100);
+console.log(x * z < 100 && x * y > 100);
